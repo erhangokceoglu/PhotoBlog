192c11bc07866f5f7cc9ed6d127aa8a1869e7ea8
John Doe <erhangokceoglu91@gmail.com>
2023-01-11 17:14:14 +0300

    Add project files.

---
+++ PhotoBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhotoBlog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts => Set<Post>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 1,
+                Title = "Mountains and Sea",
+                Description = "As the sun sets nehind the hills, I watch the unique blue of the sea.",
+                Photo = "sample.jpg"
+            });
+        }
+    }
+}
---
+++ PhotoBlog/Data/Migrations/20230111120941_Ilk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhotoBlog.Data.Migrations
+{
+    public partial class Ilk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(140)", maxLength: 140, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: true),
+                    Photo = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "CreatedTime", "Description", "Photo", "Title" },
+                values: new object[] { 1, new DateTime(2023, 1, 11, 15, 9, 41, 145, DateTimeKind.Local).AddTicks(3549), "As the sun sets nehind the hills, I watch the unique blue of the sea.", "sample.jpg", "Mountains and Sea" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
